de488a7f02c0d0727b52c00de5a19e1468ad6d06
Danila <MokinDO@chgtk.ru>
2023-05-25 13:07:30 +0800

    New commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,13 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Инженерия</a>
         <div class="nav-links">
-          {#each nav as link}
-          <a href={link.href} class="link">{link.title}</a>
-          {/each}
+            {#each nav as link}
+                {#if link.isShow}
+                 <a href={link.href} class="link">{link.title}</a>
+                {/if}
+            {/each}
+          
+
         </div>
     </div>
   </nav>
