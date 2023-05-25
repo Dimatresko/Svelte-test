b81f5b6d06d232f861800586a7fbab62a4b9d6e9
Danila <MokinDO@chgtk.ru>
2023-05-25 13:32:31 +0800

    TEST 100

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,20 @@
-<h1>Блог</h1>
-
-todo...
-
-
-
-
-<script>
-    import {routes} from './routes'
-</script>
+<svelte:head>
+  <title>Блог</title>
+</svelte:head>
+<div class='container'>
+  <h1>Наши статьи</h1>
+  <div class='blogposts'>
+    <!-- svelte-ignore missing-declaration -->
+    {#each routes as page}
+      <div class='post'>
+        <h2>{page.title}</h2>
+        <p>{page.body}</p>
+        <p class='readmore'>
+          <a class='link' href={`/blog/posts/${page.id}`}>
+            Читать далее
+          </a>
+        </p>
+      </div>
+    {/each}
+  </div>
+</div>
